6f179a8184e84e14afcfb58a77bcd1d30dd229a3
Trần Ngọc Anh <ankankbk54@gmail.com>
2024-11-16 08:49:38 +0700

    z

--- src/main/java/com/fpl/Electroland/controller/CheckoutController.java
+++ src/main/java/com/fpl/Electroland/controller/CheckoutController.java
@@ -2,28 +2,15 @@ package com.fpl.Electroland.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import com.fpl.Electroland.dao.KhachHangDAO;
 import com.fpl.Electroland.dao.LoaiKhachHangDAO;
 import com.fpl.Electroland.helper.Author;
 import com.fpl.Electroland.model.KhachHang;
-
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class CheckoutController {
