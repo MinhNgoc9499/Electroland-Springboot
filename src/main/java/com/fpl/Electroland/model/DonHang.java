601323bdd64e4a71e0135143d411fee1b70568a3
Trần Ngọc Anh <ankankbk54@gmail.com>
2024-11-26 17:06:45 +0700

    Update_Model

--- src/main/java/com/fpl/Electroland/model/DonHang.java
+++ src/main/java/com/fpl/Electroland/model/DonHang.java
@@ -4,8 +4,6 @@ import java.util.Date;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -41,26 +39,27 @@ public class DonHang {
 	@Column(nullable = false, columnDefinition = "NVARCHAR(225)") // Sử dụng NVARCHAR(225)
 	private String phuongThucTT;
 
+	@Column(nullable = false, columnDefinition = "NVARCHAR(225)") // Sử dụng NVARCHAR(225)
+	private String ghiChu;
+
 	@Temporal(TemporalType.TIMESTAMP) // Lưu cả ngày và giờ
-	@DateTimeFormat(pattern = "dd-MM-yyyy HH:mm") // Định dạng ngày tháng năm giờ:phút
-	@JsonFormat(pattern = "dd-MM-yyyy HH:mm") // Định dạng ngày tháng năm giờ:phút (dành cho JSON)
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm") // Định dạng ngày tháng năm giờ:phút
 	@Column(nullable = false)
 	private Date ngayDH = new Date(); // Ngày đơn hàng
 
 	@Temporal(TemporalType.TIMESTAMP) // Lưu cả ngày và giờ
-	@DateTimeFormat(pattern = "dd-MM-yyyy HH:mm") // Định dạng ngày tháng năm giờ:phút
-	@JsonFormat(pattern = "dd-MM-yyyy HH:mm") // Định dạng ngày tháng năm giờ:phút (dành cho JSON)
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm") // Định dạng ngày tháng năm giờ:phút
 	@Column(nullable = true)
 	private Date ngayGH; // Ngày giao hàng
 
 	@Column(nullable = false) // Trạng thái không được null
-	private int trangThai;
+	private int trangThai = 2;
+
+	@ManyToOne
+	@JoinColumn(name = "maGiamDH", nullable = true) // Khóa ngoại với MaGiamDh
+	private MaGiamDh maGiamDh;
 
 	@ManyToOne
 	@JoinColumn(name = "idKH", nullable = false) // Khóa ngoại với KhachHang
 	private KhachHang khachHang;
-
-	@ManyToOne
-	@JoinColumn(name = "idMGKH", nullable = true) // Khóa ngoại với MaGiamKh, có thể null
-	private MaGiamKh maGiamKh;
 }
