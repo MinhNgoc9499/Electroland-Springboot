c5ecd11db8b239c8aed4278246112c42a2db6043
MinhNgoc <minhngoc9499@gmail.com>
2024-12-03 12:28:19 +0700

    ddd

--- src/main/java/com/fpl/Electroland/controller/admin/OrderController.java
+++ src/main/java/com/fpl/Electroland/controller/admin/OrderController.java
@@ -1,13 +1,20 @@
 package com.fpl.Electroland.controller.admin;
 
+import com.fpl.Electroland.dao.ChiTietDhDAO;
 import com.fpl.Electroland.dao.DonHangDAO;
+import com.fpl.Electroland.model.ChiTietDh;
 import com.fpl.Electroland.model.DonHang;
 import com.fpl.Electroland.service.DonHangService;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,12 +22,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/admin/order")
 public class OrderController {
-
+    @Autowired
+    private DonHangDAO donhangDAO;
     @Autowired
     private DonHangService donHangService;
-
+    @Autowired
+    private ChiTietDhDAO chiTietDhDAO;
     @GetMapping()
-    public String orderDashboard(Model model,
+    public String orderDashboard(  @RequestParam(value="id", required = false) Integer orderId,Model model,
         @RequestParam(defaultValue = "0") int page,
         @RequestParam(defaultValue = "20") int size,
         @RequestParam(defaultValue = "") String searchOrderId,
@@ -42,7 +51,22 @@ public class OrderController {
         model.addAttribute("searchAddress", searchAddress);
         model.addAttribute("paymentMethod", paymentMethod);
         model.addAttribute("status", status);
-
+       // Lấy danh sách chi tiết đơn hàng
+       Optional<DonHang> donHang = donhangDAO.findById(orderId);
+		if (donHang.get().getTongGiam() == null) {
+			model.addAttribute("discount", "Không có giảm giá");
+		} else {
+			model.addAttribute("discount", donHang.get().getTongGiam());
+		}
+		List<ChiTietDh> chiTietDonHang = chiTietDhDAO.findByDonHangId(orderId);
+       // Tính tổng giá trị đơn hàng
+		double tongGiaTri = chiTietDonHang.stream()
+        .mapToDouble(ct -> ct.getGiaBan() * ct.getSoLuong())
+        .sum();
+        model.addAttribute("donHang", donHang);
+		model.addAttribute("chiTietDonHang", chiTietDonHang);
+		model.addAttribute("tongGiaTri", tongGiaTri);
         return "OrderList";
     }
+
 }
